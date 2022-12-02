64995c62bb13f59461a54f9a07a8e766c6e22644
Jakub <jczekanski123@gmail.com>
2022-12-02 18:25:56 +0100

    Some adjustments

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
 const nav = document.querySelector('.navbar');
-const footerYear = document.querySelector('.footer__year')
+const footerYear = document.querySelector('.footer__year');
+const navToggle = document.querySelector('.navbar-toggler');
 
-function addShadow () {
+const addShadow = () => {
     if(window.scrollY >= 200){
         nav.style.backgroundColor = "#000000"
     }else{
         nav.style.backgroundColor = 'transparent'
+    }
+}
+const showNav = () => {
+    if(navToggle.classList.contains('collapsed'))
+    {
+        nav.style.backgroundColor = "transparent"
+    }else{
+        nav.style.backgroundColor = "#000000"
     }
 }
 const handleYear = () =>{
@@ -15,4 +24,5 @@ const handleYear = () =>{
 }
 handleYear()
 window.addEventListener('scroll', addShadow)
+navToggle.addEventListener('click', showNav)
 })
